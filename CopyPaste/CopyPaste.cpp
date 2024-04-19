abd0fd4263e3515c9646486535f7e767c47940fa
_D1zast3R_ <petru00000@mail.ru>
2024-04-20 01:18:40 +0300

    Добавьте файлы проекта.

---
+++ CopyPaste/CopyPaste.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <windows.h>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream fin("data.txt");
+    string line;
+    Sleep(1000);
+    while (getline(fin, line)) {
+        for (char i : line) {
+            INPUT inputs[1] = {};
+            ZeroMemory(inputs, sizeof(inputs));
+
+            inputs[0].type = INPUT_KEYBOARD;
+            inputs[0].ki.wScan = i;
+            inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
+
+            SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+        }
+        INPUT inputs[1] = {};
+        ZeroMemory(inputs, sizeof(inputs));
+
+        inputs[0].type = INPUT_KEYBOARD;
+        inputs[0].ki.wScan = '\n';
+        inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
+
+        SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    }
+    string a;
+    cin >> a;
+    return 0;
+}
+
